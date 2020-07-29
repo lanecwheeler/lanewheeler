0f92aa4863bc9f7be9099139db958f3ae8ad5fb9
Lane Wheeler <lwheeler@liquidfish.com>
2020-07-28 20:15:53 -0500

    starting to add back in joey pepperoni

--- app/Http/Controllers/JoeyPepperoni.php
+++ app/Http/Controllers/JoeyPepperoni.php
@@ -8,11 +8,22 @@ use Illuminate\Http\Request;
 class JoeyPepperoni extends Controller
 {
     public function crcChallenge(Request $request) {
+        $crc_token = $request->get('crc_token');
+
         Log::create([
             'url' => $request->url(),
             'method' => $request->method(),
             'body' => $request->getContent(),
             'ip' => $request->ip()
         ]);
+
+        if($crc_token){
+            $hash = base64_encode(hash_hmac('sha256', $crc_token, env('TWITTER_SECRET')));
+
+            return response()->json(['response_token' => 'sha256='.$hash]);
+        }else{
+            return response('Error: crc_token is missing from request', 400);
+        }
+
     }
 }
